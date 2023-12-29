069a16cffbc24696559986eca581c1aafc5f30f9
Jaydar <jaydar@outlook.com>
2023-12-29 17:31:30 +0800

    up

--- build.rs
+++ build.rs
@@ -1,10 +1,6 @@
 
 fn main() {
-    // slint_build::compile("ui/appwindow.slint").unwrap();
-
-    let conf = slint_build::CompilerConfiguration::default()
-        .with_style("fluent".to_owned());
-    
+    let conf = slint_build::CompilerConfiguration::default().with_style("fluent".to_owned());
     slint_build::compile_with_config("ui/main.slint",conf).unwrap();
     slint_build::print_rustc_flags().unwrap();
 }
