7da8b9a3bd0b9367a032c28d262368b956f82bef
Jaydar <jaydar@outlook.com>
2024-01-08 17:32:15 +0800

    up

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] 
 
+// use bridge::bridge_init;
 use i_slint_backend_winit::WinitWindowAccessor;
 use slint::{SharedString, Color};
 use windows_sys::Win32::UI::WindowsAndMessaging::GetSystemMetrics;
@@ -7,6 +8,7 @@ use winit::dpi::LogicalPosition;
 
 mod app;
 mod tools;
+// mod bridge;
 
 slint::include_modules!();
 
@@ -32,15 +34,67 @@ async fn main() {
             ww.set_enabled_buttons(buttons ^ winit::window::WindowButtons::MAXIMIZE);
         });
    
-    main.global::<ColorComboBoxAdapter>().on_string_color(|value:SharedString| {
-        // value.
-        return Color::default();
+    main.global::<bridge>().on_string_to_color(|mut hex:SharedString| {
+
+        let default_color = Color::default();
+        
+        if  !hex.starts_with("#")  || !(hex.len()==4 || hex.len()==7 || hex.len()==9) {
+            return default_color;
+        }
+
+
+        if hex.len() == 4 {
+            hex = SharedString::from(format!("#{}{}{}{}{}{}", &hex[1..2], &hex[1..2], &hex[2..3], &hex[2..3], &hex[3..4], &hex[3..4]).to_owned())
+        };
+
+
+        let r = match u8::from_str_radix(&hex[1..3], 16) {
+            Ok(val) => val,
+            Err(_) => return default_color,
+        };
+        let g = match u8::from_str_radix(&hex[3..5], 16) {
+            Ok(val) => val,
+            Err(_) => return default_color,
+        };
+        let b = match u8::from_str_radix(&hex[5..7], 16) {
+            Ok(val) => val,
+            Err(_) => return default_color,
+        };
+ 
+        if hex.len() == 9 {
+            let a = match u8::from_str_radix(&hex[7..9], 16) {
+                Ok(val) => val,
+                Err(_) => return default_color,
+            };
+            return Color::from_argb_u8(a, r, g, b);
+
+        } else {
+            return Color::from_rgb_u8(r, g, b);
+        }
+
     });
-    main.global::<ColorComboBoxAdapter>().on_color_string(|value:Color| {
+
+    main.global::<bridge>().on_color_to_string(|value:Color| {
         let hex = format!("#{:02x}{:02x}{:02x}{:02x}", value.red(), value.green(), value.blue(), value.alpha());
         let mut ss = SharedString::default();
         ss.push_str(hex.as_str());
         return ss;
     });
+
     main.run().unwrap();
+}
+
+
+
+#[cfg(test)]
+mod tests{
+    #[test]
+    fn it_hex(){
+        let hex = "!333";
+        
+
+        println!("no #:   {}", !hex.starts_with("#") );
+        println!("no hax: {}", !(hex.len()==4 || hex.len()==7 || hex.len()==9) );
+        println!("no-hex: {}", !hex.starts_with("#")  || !(hex.len()==4 || hex.len()==7 || hex.len()==9) );
+    }
 }
